855c48adb4316f3473a8b9f0e7b82c18c72ef347
filippopendyk <filipos28511@gmail.com>
2022-12-12 15:42:40 -0500

    links tailwind to html file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'body': '#030816',
+        'selected-text': '#533483',
+        'theme': '#E94560',
+        'nav': '#868686',
+        'secondary': '#868686',
+        'badge': '#3F3F51',
+        'input-border': '#061131',
+        'input': '#061130',
+      }
+    },
   },
   plugins: [],
 }
